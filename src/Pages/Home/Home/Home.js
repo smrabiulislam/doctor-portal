a66e587d8f6bd74a6bfcb009bb6f706752ad00e0
smrabiulislam <smarbiulislam03@gmail.com>
2022-11-13 11:41:04 +0600

    first commit

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+import InfoCards from '../InfoCard/InfoCards';
+import MakeAppointment from '../MakeAppoinment/MakeAppointment';
+import Services from '../Services/Services';
+import Testimonial from '../Testimonial/Testimonial';
+
+const Home = () => {
+    return (
+        <div className='mx-5'>
+            <Banner></Banner>
+            <InfoCards></InfoCards>
+            <Services></Services>
+            <MakeAppointment></MakeAppointment>
+            <Testimonial></Testimonial>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../Layout/Main";
+import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Login";
+
+const Router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            }
+        ]
+
+    }
+])
+
+export default Router;
